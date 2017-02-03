ffa7d3351b5f915595f333af45c7bb057e31e766
k.takahashi <tk02dr@gmail.com>
2017-02-03 15:34:07 +0900

    sessionsController追加

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+
+  def new
+    # render 'new'
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+    else
+      flash.now[:danger] = 'メールアドレスまたはパスワードが正しくありません'
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+
   root 'top#home'
+
+  get    'login'  => 'sessions#new'
+  post   'login'  => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
 
   resources :users
 end
