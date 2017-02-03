906895fedbee008a661c7268cb83b90da43aed3b
k.takahashi <tk02dr@gmail.com>
2017-02-03 12:37:11 +0900

    saveに成功した時にshowViewに飛ばす

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,12 +7,18 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "登録完了"
+      redirect_to @user
     else
       render 'new'
     end
   end
 
-  def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  def show
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
