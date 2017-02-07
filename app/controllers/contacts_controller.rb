bbc6aa1fe25c7c019a15e533e0b57c97579ffcc1
k.takahashi <tk02dr@gmail.com>
2017-02-07 18:03:28 +0900

    contactのview追加

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,2 +1,37 @@
 class ContactsController < ApplicationController
+
+  def index
+  end
+
+  def new
+    @contact = Contact.new
+  end
+  def create
+    @contact = Contact.new
+    if @contact.update_attributes(contact_params)
+      flash[:success] = "作成完了"
+      redirect_to @event
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+    def contact_params
+      params.require(:contact).permit(
+          :content, :send_user_id, :receive_circle_id
+        )
+    end
+
 end
