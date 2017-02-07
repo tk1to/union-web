06c18efdf365f85538ea6fdd54a386d583a94265
k.takahashi <tk02dr@gmail.com>
2017-02-07 17:09:23 +0900

    イベント編集アクション

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -16,9 +16,21 @@ class EventsController < ApplicationController
   def show
     @event = Event.find(params[:id])
     @circle = Circle.find(@event.circle_id)
+
+    @be_member = @circle.members.include?(current_user)
   end
 
   def edit
+    @event = Event.find(params[:id])
+  end
+  def update
+    @event = Event.find(params[:id])
+    if @event.update_attributes(event_params)
+      flash[:success] = "編集完了"
+      redirect_to @event
+    else
+      render 'edit'
+    end
   end
 
   private
