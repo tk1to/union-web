6e980fb7198d5a1fcf31873346b058f3ea570699
k.takahashi <tk02dr@gmail.com>
2017-02-09 03:54:14 +0900

    フォロー関連実装完了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include SessionsHelper
+
+  private
+
+    # ユーザーのログインを確認する
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "ログインしてください。"
+        redirect_to login_url
+      end
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,11 @@
 class UsersController < ApplicationController
 
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:edit, :update, :following, :followers]
   before_action :correct_user,   only: [:edit, :update]
 
   def new
     @user = User.new
   end
-
   def create
     @user = User.new(user_params)
     if @user.save
@@ -26,7 +25,6 @@ class UsersController < ApplicationController
       redirect_to @user
     end
   end
-
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
@@ -42,20 +40,25 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def following
+    @header = "フォロー"
+    @user  = User.find(params[:id])
+    @users = @user.following
+    render 'follow'
+  end
+  def followers
+    @header = "フォロワー"
+    @user  = User.find(params[:id])
+    @users = @user.followers
+    render 'follow'
+  end
+
   private
     def user_params
       params.require(:user).permit(
           :name, :email, :password, :password_confirmation,
           :introduce, :want_to_do, :hobby,
         )
-    end
-
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
     end
 
     def correct_user
