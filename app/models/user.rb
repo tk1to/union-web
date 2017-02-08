189f0e097bc10152d97ef226629670828216fe4f
k.takahashi <tk02dr@gmail.com>
2017-02-08 20:33:48 +0900

    フォロー関連実装途中

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,11 +13,22 @@ class User < ActiveRecord::Base
   validates :want_to_do, length: { maximum: 500 }
   validates :hobby, length: { maximum: 500 }
 
+
   has_many :circles, through: :memberships
   has_many :memberships
 
   has_many :blogs
   has_many :contacts, foreign_key: "send_user_id"
+
+  #フォロー関連
+  has_many :active_relationships, class_name:  "User_relationship",
+            foreign_key: "follower_id",
+            dependent: :destroy
+  has_many :passive_relationships, class_name:  "User_relationship",
+            foreign_key: "followed_id",
+            dependent:   :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 
   # 与えられた文字列のハッシュ値を返す
   def User.digest(string)
@@ -30,13 +41,11 @@ class User < ActiveRecord::Base
   def User.new_token
     SecureRandom.urlsafe_base64
   end
-
   # 永続的セッションで使用するユーザーをデータベースに記憶する
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
   end
-
   # 渡されたトークンがダイジェストと一致したらtrueを返す
   def authenticated?(remember_token)
     return false if remember_digest.nil?
@@ -46,5 +55,18 @@ class User < ActiveRecord::Base
   # ユーザーログインを破棄する
   def forget
     update_attribute(:remember_digest, nil)
+  end
+
+  # ユーザーをフォローする
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+  # ユーザーをアンフォローする
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+  # 現在のユーザーがフォローしてたらtrueを返す
+  def following?(other_user)
+    following.include?(other_user)
   end
 end
---
+++ db/migrate/20170208111628_create_user_relationships.rb
@@ -0,0 +1,13 @@
+class CreateUserRelationships < ActiveRecord::Migration
+  def change
+    create_table :user_relationships do |t|
+      t.integer :follower_id
+      t.integer :followed_id
+
+      t.timestamps null: false
+    end
+    add_index :user_relationships, :follower_id
+    add_index :user_relationships, :followed_id
+    add_index :user_relationships, [:follower_id, :followed_id], unique: true
+  end
+end
