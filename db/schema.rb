b41382c598e0f310e968714f0780a5bd454cbcc0
k.takahashi <tk02dr@gmail.com>
2017-02-12 01:46:13 +0900

    Favoriteモデル追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170211085525) do
+ActiveRecord::Schema.define(version: 20170211164437) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,6 +73,13 @@ ActiveRecord::Schema.define(version: 20170211085525) do
     t.string   "title"
     t.text     "content"
     t.integer  "circle_id"
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer  "circle_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "memberships", force: :cascade do |t|
