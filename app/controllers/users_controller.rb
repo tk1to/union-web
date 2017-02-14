d89a0e1c0638c28089bc6c1bc6b1169d64f491e4
k.takahashi <tk02dr@gmail.com>
2017-02-14 18:44:01 +0900

    ユーザー編集画面に項目追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -60,6 +60,7 @@ class UsersController < ApplicationController
       params.require(:user).permit(
           :name, :email, :password, :password_confirmation,
           :introduce, :want_to_do, :hobby,
+          :college, :department, :grade,
           :picture,
         )
     end
