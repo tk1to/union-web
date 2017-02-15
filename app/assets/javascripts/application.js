796634856ebbf3333044f30e8d8618de60e5ac78
k.takahashi <tk02dr@gmail.com>
2017-02-15 16:46:22 +0900

    追加

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -89,6 +89,10 @@ $(function(){
   $(document).on('click', '.header__foot',function(){
     $('body').prepend($('<div>').addClass('displayed_coming_soon'));
   });
+
+  $(document).on('click', '.coming_soon_update',function(){
+    $('.displayed_coming_soon').remove();
+  });
 });
 
 
