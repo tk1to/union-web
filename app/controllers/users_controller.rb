f447c08fa2834a402e805a566e02eaae118ab407
k.takahashi <tk02dr@gmail.com>
2017-02-16 03:06:28 +0900

    エラー文

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
       flash[:info] = "アカウントを有効にするためにメールを確認してください。"
       redirect_to root_url
     else
+      flash.now[:error] = "エラーがあります。"
       render 'new'
     end
   end
