084b8e83617277b8f4fcd7f7fab5fd06a53fbdfc
k.takahashi <tk02dr@gmail.com>
2017-02-20 16:24:10 +0900

    session_helper無効

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,14 +1,14 @@
 module SessionsHelper
 
-  def log_in(user)
-    session[:user_id] = user.id
-  end
+  # def log_in(user)
+  #   session[:user_id] = user.id
+  # end
 
-  def remember(user)
-    user.remember
-    cookies.permanent.signed[:user_id] = user.id
-    cookies.permanent[:remember_token] = user.remember_token
-  end
+  # def remember(user)
+  #   user.remember
+  #   cookies.permanent.signed[:user_id] = user.id
+  #   cookies.permanent[:remember_token] = user.remember_token
+  # end
 
   # def current_user
   #   if user_id = session[:user_id]
