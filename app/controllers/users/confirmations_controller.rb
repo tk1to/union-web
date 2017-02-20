e1a2257f45954c7d6243b7a93208b948ac839fe2
k.takahashi <tk02dr@gmail.com>
2017-02-21 00:10:32 +0900

    メール認証後の自動ログイン

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -25,6 +25,7 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
   # The path used after confirmation.
   def after_confirmation_path_for(resource_name, resource)
     # super(resource_name, resource)
+    sign_in(resource)
     :root
   end
 end
