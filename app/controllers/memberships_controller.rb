58fb74b980c0ab5844d787e368cf71d16cf3a554
k.takahashi <tk02dr@gmail.com>
2017-03-04 22:42:20 +0900

    編集者更新機能

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -3,6 +3,11 @@ class MembershipsController < ApplicationController
   before_action :authenticate_user!
 
   def status
+    @circle     = Circle.find(params[:id])
+    memberships = @circle.memberships
+    @chiefs     = memberships.chief
+    @admins     = memberships.admin
+    @editors    = memberships.editor
   end
 
   def status_edits
@@ -19,13 +24,17 @@ class MembershipsController < ApplicationController
   def admin_update
     @circle = Circle.find(params[:id])
     if params[:admin_update_confirmed]
-      added_admin_ids   = params[:added_admin].keys.map{|n|n.to_i}
-      removed_admin_ids = params[:removed_admin].keys.map{|n|n.to_i}
-
-      added_admin_ids.each{|id|Membership.find(id).admin!}
-      removed_admin_ids.each{|id|Membership.find(id).ordinary!}
+      if params[:added_admin]
+        added_admin_ids   = params[:added_admin].keys.map{|n|n.to_i}
+        added_admin_ids.each{|id|Membership.find(id).admin!}
+      end
+      if params[:removed_admin]
+        removed_admin_ids = params[:removed_admin].keys.map{|n|n.to_i}
+        removed_admin_ids.each{|id|Membership.find(id).ordinary!}
+      end
+      redirect_to [:status, @circle]
     else
-      submitted_membership_ids = params[:admin_edit].keys.map{|n|n.to_i}
+      submitted_membership_ids = params[:admin_edit] ? params[:admin_edit].keys.map{|n|n.to_i} : []
       current_admin_ids = @circle.memberships.admin.ids
 
       added_admin_ids   = submitted_membership_ids.select{|id|!current_admin_ids.include?(id)}
@@ -34,6 +43,36 @@ class MembershipsController < ApplicationController
       removed_admin_ids = current_admin_ids.select{|id|!submitted_membership_ids.include?(id)}
       @removed_admins   = removed_admin_ids.map{|id|Membership.find(id)}
       render "admin_edit_confirm"
+    end
+  end
+
+  def editor_edit
+    @circle  = Circle.find(params[:id])
+    @members = @circle.members
+    @memberships = @circle.memberships.select{|ms|!ms.chief? && !ms.admin?}
+  end
+  def editor_update
+    @circle = Circle.find(params[:id])
+    if params[:editor_update_confirmed]
+      if params[:added_editor]
+        added_editor_ids   = params[:added_editor].keys.map{|n|n.to_i}
+        added_editor_ids.each{|id|Membership.find(id).editor!}
+      end
+      if params[:removed_editor]
+        removed_editor_ids = params[:removed_editor].keys.map{|n|n.to_i}
+        removed_editor_ids.each{|id|Membership.find(id).ordinary!}
+      end
+      redirect_to [:status, @circle]
+    else
+      submitted_membership_ids = params[:editor_edit] ? params[:editor_edit].keys.map{|n|n.to_i} : []
+      current_editor_ids = @circle.memberships.editor.ids
+
+      added_editor_ids   = submitted_membership_ids.select{|id|!current_editor_ids.include?(id)}
+      @added_editors     = added_editor_ids.map{|id|Membership.find(id)}
+
+      removed_editor_ids = current_editor_ids.select{|id|!submitted_membership_ids.include?(id)}
+      @removed_editors   = removed_editor_ids.map{|id|Membership.find(id)}
+      render "editor_edit_confirm"
     end
   end
 
