a23168997da1794972b2b31b55e0e600605c0a35
k.takahashi <tk02dr@gmail.com>
2017-03-05 13:31:40 +0900

    気になるAjax対応

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,19 +3,20 @@ class FavoritesController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    circle = Circle.find(params[:circle_id])
-    # current_user.favorites.create(circle_id: circle.id)
+    @circle = Circle.find(params[:circle_id])
+    current_user.favorites.create(circle_id: @circle.id)
     respond_to do |format|
       format.html { redirect_to :top }
       format.js
     end
-    end
   end
 
   def destroy
-    circle = Circle.find(params[:circle_id])
-    Favorite.find(params[:id]).destroy
-    flash[:success] = "気になるを解除しました"
-    redirect_to circle
+    @circle = Circle.find(params[:circle_id])
+    Favorite.find_by(circle_id: params[:circle_id], user_id: current_user.id).destroy
+    respond_to do |format|
+      format.html { redirect_to :top }
+      format.js
+    end
   end
 end
