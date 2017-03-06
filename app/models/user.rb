3bc1fca0d79b28b6a6a40e0836291dd7ebe40dfd
k.takahashi <tk02dr@gmail.com>
2017-03-07 03:40:09 +0900

    バリデーション解除

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,6 @@ class User < ActiveRecord::Base
   before_save :downcase_email
 
   validates :name, length: { maximum: 50 }
-  validates :password, presence: true, length: { minimum: 6, maximum: 24 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence:   true, length: { maximum: 255 },
                     format:     { with: VALID_EMAIL_REGEX },
