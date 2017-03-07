e7aff0ddaf10ee2c4c2ed4d53854ea6303e84031
k.takahashi <tk02dr@gmail.com>
2017-03-07 13:40:21 +0900

    URL加入ルート

--- app/controllers/web_controller.rb
+++ app/controllers/web_controller.rb
@@ -17,9 +17,27 @@ class WebController < ApplicationController
     if params[:key].length == 9
       keys = params[:key].chars
       circle_id_from_key = decipher(keys[6])*1000 + decipher(keys[7])*100 + decipher(keys[2])*10 + decipher(keys[4])
-      session["joining_circle_id"] = circle_id_from_key
-      redirect_to [:new, :user, :registration]
+      if circle = Circle.find_by(id: circle_id_from_key)
+        if user_signed_in?
+          circle.memberships.create(member_id: current_user.id, status: 3)
+          flash[:success] = "#{circle.name}に加入しました"
+          redirect_to :top
+        else
+          flash[:notice] = "登録後に#{circle.name}に加入されます"
+          session["joining_circle_id"] = circle_id_from_key
+          redirect_to [:new, :user, :registration]
+          return
+        end
+      else
+        flash[:failure] = "有効なURLではありませんでした"
+        redirect_to :top
+        return
+      end
     else
+      flash[:failure] = "有効なURLではありませんでした"
+      redirect_to :top
+      return
     end
   end
+
 end
