5ac97cdd8a4cf896214084c388d3c88550171b66
k.takahashi <tk02dr@gmail.com>
2017-03-08 12:02:59 +0900

    c_pictureクラス

--- app/controllers/web_controller.rb
+++ app/controllers/web_controller.rb
@@ -12,23 +12,28 @@ class WebController < ApplicationController
   end
 
   include MemberKeyHelper
-  def circle_key
-    if params[:key].length == 9
-      keys = params[:key].chars
-      circle_id_from_key = deciph(keys)
-      if circle = Circle.find_by(id: circle_id_from_key)
-        if user_signed_in?
-          if !circle.memberships.find_by(member_id: current_user)
-            circle.memberships.create(member_id: current_user.id, status: 3)
-            flash[:success] = "#{circle.name}に加入しました"
+    def circle_key
+      if params[:key].length == 9
+        keys = params[:key].chars
+        circle_id_from_key = deciph(keys)
+        if circle = Circle.find_by(id: circle_id_from_key)
+          if user_signed_in?
+            if !circle.memberships.find_by(member_id: current_user)
+              circle.memberships.create(member_id: current_user.id, status: 3)
+              flash[:success] = "#{circle.name}に加入しました"
+            else
+              flash[:success] = "既に#{circle.name}のメンバーです"
+            end
+            redirect_to :top
           else
-            flash[:success] = "既に#{circle.name}のメンバーです"
+            flash[:notice] = "登録後に#{circle.name}に加入されます"
+            session["joining_circle_id"] = circle_id_from_key
+            redirect_to [:new, :user, :registration]
+            return
           end
-          redirect_to :top
         else
-          flash[:notice] = "登録後に#{circle.name}に加入されます"
-          session["joining_circle_id"] = circle_id_from_key
-          redirect_to [:new, :user, :registration]
+          flash[:failure] = "有効なURLではありませんでした"
+          redirect_to :top
           return
         end
       else
@@ -36,11 +41,6 @@ class WebController < ApplicationController
         redirect_to :top
         return
       end
-    else
-      flash[:failure] = "有効なURLではありませんでした"
-      redirect_to :top
-      return
     end
-  end
 
 end
