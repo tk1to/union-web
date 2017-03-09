0aac393290f96d5d5fc148b5a2c957f3b30a99ae
k.takahashi <tk02dr@gmail.com>
2017-03-09 22:39:11 +0900

    ga導入

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,9 @@ Rails.application.configure do
     :enable_starttls_auto => true
   }
 
+  # google analytics
+  GA.tracker = "UA-71539217-2"
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
