b7abf29c86d529fcb9f3c8682bf1804c2b40bac4
k.takahashi <tk02dr@gmail.com>
2017-03-12 15:18:18 +0900

    都道府県

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,11 @@ class UsersController < ApplicationController
         for i in 0..Category.max-1 do
           @category_ids[i] = @categories[i].nil? ? nil : @categories[i].id
         end
+      elsif @edit_item == "birth_place" || @edit_item == "home_place"
+        @prefectures = prefectures
       end
+    else
+      @prefectures = prefectures
     end
   end
   def update
@@ -142,6 +146,18 @@ class UsersController < ApplicationController
           end
         end
       end
+    end
+    def prefectures
+      ["北海道", "青森県", "岩手県", "宮城県", "秋田県",
+      "山形県", "福島県", "茨城県", "栃木県", "群馬県",
+      "埼玉県", "千葉県", "東京都", "神奈川県", "新潟県",
+      "富山県", "石川県", "福井県", "山梨県", "長野県",
+      "岐阜県", "静岡県", "愛知県", "三重県", "滋賀県",
+      "京都府", "大阪府", "兵庫県", "奈良県", "和歌山県",
+      "鳥取県", "島根県", "岡山県", "広島県", "山口県",
+      "徳島県", "香川県", "愛媛県", "高知県", "福岡県",
+      "佐賀県", "長崎県", "熊本県", "大分県", "宮崎県",
+      "鹿児島県", "沖縄県"]
     end
 
 end
