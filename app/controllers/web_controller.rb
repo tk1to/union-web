5498d3eb4aa33b2765510c25c33a4f7c653a88a2
k.takahashi <tk02dr@gmail.com>
2017-03-16 16:14:03 +0900

    ナビゲーション追加

--- app/controllers/web_controller.rb
+++ app/controllers/web_controller.rb
@@ -14,9 +14,6 @@ class WebController < ApplicationController
       @tutorialing = true
       current_user.update_attribute(:tutorialed, true)
     end
-
-    @tutorialing = true
-
   end
 
   def landing
