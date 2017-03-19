71944e4eb779fc24c95cb889573339d3a378691f
k.takahashi <tk02dr@gmail.com>
2017-03-20 05:59:10 +0900

    通知新着にクラス

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -6,8 +6,11 @@ class NotificationsController < ApplicationController
     @notifications = Notification.where(hold_user_id: current_user).order("created_at DESC")
 
     current_user.update_attributes(new_notifications_count: 0, new_notifications_exist: false)
+
+    @checking_notification_ids = []
     @notifications.where(checked: false).each do |notification|
       notification.update_attribute(:checked, true)
+      @checking_notification_ids << notification.id
     end
   end
 end
